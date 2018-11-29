97b69f42c1698192dbd957cc7d662450837ea0d8
Shannon Harris <sr60621@gmail.com>
2018-11-29 12:15:37 -0600

    Add render return SearchPage

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class BooksApp extends React.Component {
     return(
       <div>
         <Route exact path="/" component={ MainPage } />
+        <Route exact path="/search" component={ SearchPage } />
       </div>
     );
 
