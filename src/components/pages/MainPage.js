f02e1180e0913e1b4eddfac16febe13b9f5b8d15
Shannon Harris <sr60621@gmail.com>
2018-11-29 12:34:06 -0600

    Change <a> to <Link to=>

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -159,7 +159,7 @@ class MainPage extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link to="/search">Add a book</Link>
             </div>
           </div>
         );
