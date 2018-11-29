522ba776b73ab2f21c01945f73b404bbce9d2658
Shannon Harris <sr60621@gmail.com>
2018-11-29 13:21:33 -0600

    Adding additional Shelves

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -13,6 +13,8 @@ class MainPage extends React.Component {
             <div className="list-books-content">
               <div>
                 
+              <Shelf />
+              <Shelf />
               <Shelf />  
               
               </div>
