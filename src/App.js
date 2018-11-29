73d8910a48a462a99f7194302941815e74b04cf2
Shannon Harris <sr60621@gmail.com>
2018-11-29 11:53:41 -0600

    Import Route from react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
+import { Route } from 'react-router-dom'
+
 class BooksApp extends React.Component {
   state = {
     /**
