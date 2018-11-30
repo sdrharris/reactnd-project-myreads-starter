427e766e8d85ccd1d463af5fcaa2c74f90a9276f
Shannon Harris <sr60621@gmail.com>
2018-11-29 20:59:18 -0600

    Add constructor and change props to state

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,6 +4,13 @@ import { Link } from 'react-router-dom';
 
 
 class Book extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            book: ...props.book
+        }
+    }
+
     componentDidMount() {
         console.log(this);
     }
@@ -20,9 +27,9 @@ class Book extends React.Component {
             <li>
               <div className="book">
                 <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${this.props.book.imageLinks && this.props.book.imageLinks.thumbnail || ""}")` }}></div>
+                  <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${this.state.book.imageLinks && this.state.book.imageLinks.thumbnail || ""}")` }}></div>
                   <div className="book-shelf-changer">
-                    <select value={this.props.book.shelf || "none"}>
+                    <select value={this.state.book.shelf || "none"}>
                       <option value="move" disabled>Move to...</option>
                       <option value="currentlyReading">Currently Reading</option>
                       <option value="wantToRead">Want to Read</option>
@@ -31,8 +38,8 @@ class Book extends React.Component {
                     </select>
                   </div>
                 </div>
-                <div className="book-title">{this.props.book.title}</div>
-                <div className="book-authors">{this.props.book.authors[0] || "No Author..."}</div>
+                <div className="book-title">{this.state.book.title}</div>
+                <div className="book-authors">{this.state.book.authors[0] || "No Author..."}</div>
               </div>
             </li>
         );
