127ffc7aa21cdcc06eb64c1aaea203de0742b6b4
Shannon Harris <sr60621@gmail.com>
2018-11-30 09:50:44 -0600

    Correct errors

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,25 +4,14 @@ import { Link } from 'react-router-dom';
 import * as BooksAPI from '../BooksAPI';
 
 class Book extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            book: props.book
-        }
-    }
-
-    componentDidMount() {
-        console.log(this);
-    }
-
     render() {
         return (
             <li>
               <div className="book">
                 <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${this.state.book.imageLinks && this.state.book.imageLinks.thumbnail || ""}")` }}></div>
+                  <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${this.props.book.imageLinks && this.props.book.imageLinks.thumbnail || ""}")` }}></div>
                   <div className="book-shelf-changer">
-                    <select value={this.state.book.shelf || "none"} onChange={(e) => { this.updateBook(e.target.value) }}>
+                    <select value={this.props.book.shelf || "none"} onChange={(e) => { this.props.updateBook(this.props.book, e.target.value) }}>
                       <option value="move" disabled>Move to...</option>
                       <option value="currentlyReading">Currently Reading</option>
                       <option value="wantToRead">Want to Read</option>
@@ -31,8 +20,8 @@ class Book extends React.Component {
                     </select>
                   </div>
                 </div>
-                <div className="book-title">{this.state.book.title}</div>
-                <div className="book-authors">{this.state.book.authors[0] || "No Author..."}</div>
+                <div className="book-title">{this.props.book.title}</div>
+                <div className="book-authors">{this.props.book.authors[0] || "No Author..."}</div>
               </div>
             </li>
         );
