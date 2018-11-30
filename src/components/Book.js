7a3369ff2a9423e56de6542b5a9477ffc71afb1b
Shannon Harris <sr60621@gmail.com>
2018-11-29 20:29:43 -0600

    Assign shelf property to select

--- src/components/Book.js
+++ src/components/Book.js
@@ -15,7 +15,7 @@ class Book extends React.Component {
                 <div className="book-top">
                   <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${this.props.book.imageLinks && this.props.book.imageLinks.thumbnail || ""}")` }}></div>
                   <div className="book-shelf-changer">
-                    <select>
+                    <select value={this.props.book.shelf || "none"}>
                       <option value="move" disabled>Move to...</option>
                       <option value="currentlyReading">Currently Reading</option>
                       <option value="wantToRead">Want to Read</option>
